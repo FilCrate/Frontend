afe4c98353f82eaef16f583921f6055e56cdc705
nhi-d-tran <trndnh164@gmail.com>
2018-04-11 14:30:19 -0400

    Update design

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Routes from './Routes';
 import Footer from './components/Footer';
+import Home from './components/Home';
 import './App.css';
+
+import Myaccount from './components/Myaccount';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Routes />
-        <Footer />
+	        <Header />
+        <Switch>
+	        <Route exact path="/"  component= {Home} />
+	        <Route exact path="/Myaccount"  component= {Myaccount} />
+	        <Redirect to="/" />
+      </Switch>
+
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import '../styles/Header.css';
 
+import 'font-awesome/css/font-awesome.min.css';
+
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
+
+import Myaccount from './Myaccount';
+
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+        	<div className = "header-layout">
+        		<div className = "container">
+                    <img className ="logo" src = {require('../images/logo.png')} />
+                    <a className="feature1" href="#">Crate</a>
+                    <a className="feature2" href="#">Stores</a>
+                    <a className="feature3" href="/Myaccount">My Account</a>
+                    <a className="feature4" href="#"> <i className ="fa fa-shopping-cart"></i> Cart</a>      
+                </div>
+        		
+        	</div>
+
         );
     }
 }
