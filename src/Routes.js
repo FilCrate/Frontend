3006077253ab30d8ebb3e7ce280303f1c1f1e0c3
Chris Panican <chrispanican@gmail.com>
2018-05-02 18:28:14 -0400

    Skeleton structure for product page

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import ProductPage from './components/ProductPage';
 
 class Routes extends Component {
     render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
                 <Switch>
                     <Route exact path="/"  component= {Home} />
                     <Route exact path="/login"  component= {Login} />
+                    <Route path='/product/:number' component={ProductPage}/>
                 </Switch>
             </main>
         );
---
+++ src/components/Reviews.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import '../styles/Reviews.css';
+
+
+class Reviews extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            readyState: 'pending',
+            visibility: 'hidden',
+            ratings: [],
+        }
+        this.getRating = this.getRating.bind(this);
+    }
+
+    componentDidMount() {
+        this.getRating();
+    }
+
+    getRating() {
+        if (this.props.productId) {
+            this.setState({
+                readyState: 'loading'
+            });
+            console.log("ADFASDF");
+            fetch(`http://localhost:8000/reviews/${this.props.productId}`).then(response => {
+                console.log(response);
+                if (!response.ok) {
+                    throw new Error();
+                }
+                return response.json();
+            }).then(json => {
+                this.setState({
+                    ratings: json,
+                    readyState: 'fulfilled'
+                });
+            }).catch(error => {
+                this.setState({
+                    readyState: 'rejected'
+                })
+            })
+        }
+    }
+
+    render () {
+        return (
+            <div className="Rating">
+                <h2>Reviews</h2>
+                {(this.state.readyState === 'pending' || this.state.readyState === 'loading') && (
+                    <p>Loading...</p>
+                )}
+                {this.state.readyState === 'fulfilled' && (
+                    <div>
+                        {/* <p>No ratings</p> Map into the ratings. No ratings if len = 0 */}
+                        <p>No ratings</p>
+                    </div>
+                )}
+                {(this.state.readyState === 'rejected') && (
+                    <p>Error loading ratings for this product.</p>
+                )}
+                {console.log(this)}
+            </div>
+        )
+    }
+}
+
+
+export default Reviews;
