4398caedbcb073a9122a8f253774e056b03892a9
Chris Panican <chrispanican@gmail.com>
2018-05-08 15:54:56 -0400

    Fix merge conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,23 +34,13 @@ class Header extends Component {
         let sColor = `rgb(${x.color},${x.color},${x.color})`;
         return (
         	<div style={{backgroundColor: bg}} className="header-layout">
-<<<<<<< HEAD
-        		<navbar className="header-container navbar navbar-expand-lg navbar-light bg-light">
-                    <a href="/"><img className ="header-logo nav-link" src={require('../images/logo.png')} /></a>
-                    <a style={{color: sColor}} className="header-features nav-link" href="/crates">Crates</a>
-                    <a style={{color: sColor}} className="header-features nav-link" href="#">Stores</a>
-                    <a style={{color: sColor}} className="header-features nav-link" href="/login">My Account</a>
-                    <a style={{color: sColor}} className="header-features nav-link" href="#"> <i className ="fa fa-shopping-cart"></i> Cart</a>      
-                </navbar>
-=======
         		<div className="header-container">
                     <a href="/"><img className ="header-logo" src={require('../images/logo.png')} /></a>
-                    <a style={{color: sColor}} className="header-features" href="#">Crates</a>
+                    <a style={{color: sColor}} className="header-features" href="/crates">Crates</a>
                     <a style={{color: sColor}} className="header-features" href="/store">Stores</a>
                     <a style={{color: sColor}} className="header-features" href="/login">My Account</a>
                     <a style={{color: sColor}} className="header-features" href="#"> <i className ="fa fa-shopping-cart"></i> Cart</a>      
                 </div>
->>>>>>> b1d37ab24199d71ad048037eaaeb254f7bb12da8
         	</div>
         );
     }
